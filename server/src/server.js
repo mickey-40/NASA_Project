3d204e627481297c09793eef07b9a94f122f25da
mickey-40 <mickeyarnold53@yahoo.com>
2024-12-02 18:40:22 -0600

    database connected

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 
@@ -8,9 +9,20 @@ const {
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = 'mongodb+srv://admin:<password>@cluster0.llqy0.mongodb.net/nasa?retryWrites=true&w=majority&appName=Cluster0'
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', ()=>{
+  console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+})
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
 
   server.listen(PORT, () => {
